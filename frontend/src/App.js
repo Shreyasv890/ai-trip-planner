0e830045fb105fdc7318187f661ec0e9caa3b6fc
Shreyasv890 <shreyasv640@gmail.com>
2026-05-25 14:01:40 +0530

    updated frontend app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,7 @@
 import { useState, useEffect, useRef } from "react";
- const API = "https://ai-trip-backend-oxo5.onrender.com" ;
+
+const API = "https://ai-trip-backend-oxo5.onrender.com/api";
+
 
 async function callGroq(prompt) {
   const res = await fetch(`${API}/auth/generate`, {
